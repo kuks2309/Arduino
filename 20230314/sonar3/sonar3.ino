903b3f269815174a77381c247198ec18c4695364
kuks2309 <70010841+kuks2309@users.noreply.github.com>
2023-03-14 15:36:54 +0900

    Add files via upload

---
+++ 20230314/sonar3/sonar3.ino
@@ -0,0 +1,38 @@
+#define TRIG1 3  // 초음파 센서 1번 Trig 핀 번호
+#define ECHO1 4  // 초음파 센서 1번 Echo 핀 번호
+
+#define TRIG2 5  // 초음파 센서 2번 Trig 핀 번호
+#define ECHO2 6  // 초음파 센서 2번 Echo 핀 번호
+
+#define TRIG3 7  // 초음파 센서 3번 Trig 핀 번호
+#define ECHO3 8  // 초음파 센서 3번 Echo 핀 번호
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1,  INPUT);
+  Serial.begin(115200); // 통신속도를 115200으로 정의함   
+}
+
+long sonar1(void)  // 초음파 센서 1번 측정 함수
+{
+  long duration, distance;
+  digitalWrite(TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG1, LOW);
+  duration = pulseIn(ECHO1, HIGH);
+  distance = ( (float)(340*duration)/1000)/2;
+  return distance;
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+ 
+  //Serial.print("Duration: ");
+  //Serial.println(duration);
+  Serial.print("Distance1: ");
+  Serial.println( sonar1() );
+  delay(500);
+}
